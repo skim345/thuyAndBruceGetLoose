7f66e84a84b1aa0e6c699902043dc354f906164f
Stephanie Kim <skim345@gmail.com>
2017-02-03 12:09:21 -0800

    updated pictures and contact information section

--- client/assets/app.js
+++ client/assets/app.js
@@ -26,54 +26,6 @@ myApp.config(function($routeProvider, $locationProvider){
 	.when('/pictures', {
 	templateUrl: '/partials/pictures.html'
 	})
-	.when('/pic1', {
-	templateUrl: '/partials/engagement1.html'
-	})
-	.when('/pic2', {
-	templateUrl: '/partials/engagement2.html'
-	})
-	.when('/pic3', {
-	templateUrl: '/partials/engagement3.html'
-	})
-	.when('/pic4', {
-	templateUrl: '/partials/engagement4.html'
-	})
-	.when('/pic5', {
-	templateUrl: '/partials/engagement5.html'
-	})
-	.when('/pic6', {
-	templateUrl: '/partials/engagement6.html'
-	})
-	.when('/pic7', {
-	templateUrl: '/partials/engagement7.html'
-	})
-	.when('/pic8', {
-	templateUrl: '/partials/engagement8.html'
-	})
-	.when('/pic9', {
-	templateUrl: '/partials/engagement9.html'
-	})
-	.when('/pic10', {
-	templateUrl: '/partials/engagement10.html'
-	})
-	.when('/pic11', {
-	templateUrl: '/partials/engagement11.html'
-	})
-	.when('/pic12', {
-	templateUrl: '/partials/engagement12.html'
-	})
-	.when('/pic13', {
-	templateUrl: '/partials/engagement13.html'
-	})
-	.when('/pic14', {
-	templateUrl: '/partials/engagement14.html'
-	})
-	.when('/pic15', {
-	templateUrl: '/partials/engagement15.html'
-	})	
-	.when('/pic16', {
-	templateUrl: '/partials/engagement16.html'
-	})
 	.otherwise({
 		templateUrl: '/partials/main.html'
 	})
